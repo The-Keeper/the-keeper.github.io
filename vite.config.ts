66f225fb703d9321a8f3ad86950d4a845dff2e52
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-09-05 01:12:05 +0500

    version bump and three minimifier

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],  
+	plugins: [{ ...threeMinifier(), enforce: "pre" }, sveltekit(), purgeCss()],  
 	ssr: {
 		noExternal: ['three']
 	}
