256b8f34f124ec7e967c39d8ab8f8ba9268b2c5d
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-09-05 15:46:46 +0500

    items circle

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
   import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls, Sky } from '@threlte/extras'
+  import { ContactShadows, Grid, OrbitControls, Sky } from '@threlte/extras'
+
+  const circle = [6, 10, 16, 20].map(x => x*7);
+  const circle_gap_mult = 4;
+  const initial_radius = 4;
+  const box_size = 0.2;
+  const arch_circle = 0.9;  // 1 is full;
+  const items_elevation = 1;
 </script>
 
 <Sky elevation={0.5} />
 <T.PerspectiveCamera
   makeDefault
   position={[0, 3, 18]}
-  fov={60}
+  fov={10}
   on:create={({ ref }) => {
     ref.lookAt(0, 0, 0)
   }}
@@ -16,8 +23,8 @@
 
 <T.PerspectiveCamera
   makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
+  position={[-50, 50, 50]}
+  fov={10}
 >
   <OrbitControls
     autoRotate
@@ -44,42 +51,13 @@
   opacity={0.5}
 />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+{#each circle as items_per_circle, circle_i} 
+{   #each Array(items_per_circle) as _, instance_j}
+      <T.Mesh
+        position = {[Math.sin(Math.PI * 2 * arch_circle * instance_j / items_per_circle) * (circle_i * circle_gap_mult + initial_radius), items_elevation, Math.cos(Math.PI * 2 * arch_circle * instance_j / items_per_circle) *  (circle_i * circle_gap_mult + initial_radius)]}
+        >
+        <T.BoxGeometry args={[box_size, box_size, box_size]} />
+        <T.MeshStandardMaterial color="#00FF00" />
+      </T.Mesh>
+  {/each}
+{/each}
