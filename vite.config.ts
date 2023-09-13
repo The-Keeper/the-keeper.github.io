3d43b08838aeedd923490b31f4326ff7992f5bb6
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-09-13 05:07:53 +0500

    revert config file changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [{ ...threeMinifier(), enforce: "pre" }, sveltekit(), purgeCss()],  
-	ssr: {
-		noExternal: ['three']
-	}
+	plugins: [sveltekit(), purgeCss()]
 });
