01255abf646d7183677e226cea6a9be0e45129db
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-30 18:34:04 +0500

    update svelte and move to uno

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,14 @@
-<script lang='ts'>
-	import '../app.postcss';
+<script lang="ts">
+	import '../app.css';
+	import "@unocss/reset/tailwind.css";
+	import "virtual:uno.css";
 
-	// Components & Utilities
-	import { AppShell, Modal, Toast, initializeStores, prefersReducedMotionStore } from '@skeletonlabs/skeleton';
-	initializeStores();
+	const { children } = $props();
 
-	import Drawer from '$lib/components/Drawer.svelte';
-	import CustomAppBar from '$lib/components/CustomAppBar.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { base } from '$app/paths';
 
-	// Disable left sidebar on homepage
-	$: allowPageSmoothScroll = !$prefersReducedMotionStore ? 'scroll-smooth' : '';
-</script>
+ </script>
 
-<!-- Overlays -->
-<Toast />
-<Drawer />
-
-<!-- App Shell -->
-<AppShell regionPage={allowPageSmoothScroll} slotFooter="bg-black p-4">
-	<!-- Header -->
-	<svelte:fragment slot="header">
-		<CustomAppBar />
-	</svelte:fragment>
-
-	<!-- Sidebar (Left) -->
-	<svelte:fragment slot="sidebarLeft">
-		<div id="sidebar-left" class="hidden lg:grid w-[360px] h-full overflow-hidden"><Sidebar/></div>
-	</svelte:fragment>
-
-	<!-- Page Content -->
-	<slot />
-
-	<!-- Page Footer -->
-	<svelte:fragment slot="pageFooter">
-		<Footer />
-	</svelte:fragment>
-</AppShell>
+	<main class="md:ml-64 p-4">
+		{@render children()}
+	</main>
