4662c7f9477389c228e5e95e39a070f8ed46d329
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-30 19:01:14 +0500

    text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
 	<h1 class="ms-10 me-10 text-2xl text-center">The-Keeper</h1>
 
 	<div class="ms-6 me-6 flex justify-center">
-		<p class="">
-			Welcome to my personal home page!
-		</p>
+		<div class="flex-col">
+			<p class="">
+				Welcome to my personal home page!
+			</p>
+			<p class="">
+				More content to follow soon...
+			</p>
+		</div>
 	</div>
-
 </div>
